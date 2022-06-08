d483916d17bf2a91b2cd06d0750be9bd1116d52d
zebwu <zebwu@tencent.com>
2022-06-08 23:16:46 +0800

    feat: 修改端口

--- film_api/db/db.js
+++ film_api/db/db.js
@@ -4,7 +4,7 @@ const conn = mysql.createConnection({
    host:'localhost',    //数据库地址
    user:'root', //用户名
    password:'123456', //密码
-   database:'db_film' //数据库名
+   database:'film' //数据库名
 });
 conn.connect();
 module.exports = conn;
